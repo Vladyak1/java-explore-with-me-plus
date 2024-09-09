91303f4fb2213e643504a0e1e9448c1ba3eb4a4a
Vlad Yakovlev <vkysnoteevo1@mail.ru>
2024-09-09 20:08:15 +0300

    Правки по ревью

--- ewm-stat-service/stat-server/src/main/java/ru/practicum/mappper/HitMapper.java
+++ ewm-stat-service/stat-server/src/main/java/ru/practicum/mappper/HitMapper.java
@@ -6,7 +6,7 @@ import ru.practicum.dto.EndpointHit;
 import ru.practicum.model.Hit;
 
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = org.mapstruct.MappingConstants.ComponentModel.SPRING)
 public interface HitMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "timestamp", dateFormat = "yyyy-MM-dd HH:mm:ss")
--- ewm-stat-service/stat-server/src/main/java/ru/practicum/repository/HitRepository.java
+++ ewm-stat-service/stat-server/src/main/java/ru/practicum/repository/HitRepository.java
@@ -15,15 +15,15 @@ public interface HitRepository extends JpaRepository<Hit, Long> {
 
     @Query("select new ru.practicum.dto.ViewStats(h.app, h.uri, count (distinct h.ip)) " +
             "from Hit h " +
-            "where h.timestamp between ?1 and ?2 " +
+            "where h.timestamp between :start and :end " +
             "group by h.app, h.uri " +
             "order by count (distinct h.ip) desc")
     List<ViewStats> findAllUniqueHitsWhenUriIsEmpty(LocalDateTime start, LocalDateTime end);
 
     @Query("select new ru.practicum.dto.ViewStats(h.app, h.uri, count (distinct h.ip)) "
             + "from Hit h "
-            + "where h.timestamp between ?1 and ?2 "
-            + "and h.uri in (?3)"
+            + "where h.timestamp between :start and :end "
+            + "and h.uri in (:uris)"
             + "group by h.app, h.uri "
             + "order by count (distinct h.ip) desc ")
     List<ViewStats> findAllUniqueHitsWhenUriIsNotEmpty(
@@ -31,15 +31,15 @@ public interface HitRepository extends JpaRepository<Hit, Long> {
 
     @Query("select new ru.practicum.dto.ViewStats(h.app, h.uri, count (h.ip))" +
             "from Hit h " +
-            "where h.timestamp between ?1 and ?2 " +
+            "where h.timestamp between :start and :end " +
             "group by h.app, h.uri " +
             "order by count (h.ip) desc")
     List<ViewStats> findAllHitsWhenUriIsEmpty(LocalDateTime start, LocalDateTime end);
 
     @Query("select new ru.practicum.dto.ViewStats(h.app, h.uri, count (h.ip)) "
             + "from Hit h "
-            + "where h.timestamp between ?1 and ?2 "
-            + "and h.uri in (?3)"
+            + "where h.timestamp between :start and :end "
+            + "and h.uri in (:uris)"
             + "group by h.app, h.uri "
             + "order by count (h.ip) desc")
     List<ViewStats> findAllHitsWhenStarEndUris(LocalDateTime start, LocalDateTime end, List<String> uris);
