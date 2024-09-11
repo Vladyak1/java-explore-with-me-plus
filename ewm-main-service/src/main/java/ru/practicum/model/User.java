d1fb536217ed3e651f2d18770e4d9ae951a6ca1e
Степан Бокарев <bokarevstepan1@yandex.ru>
2024-09-11 22:59:46 +0300

    Добавить структуру

---
+++ ewm-main-service/src/main/java/ru/practicum/model/User.java
@@ -0,0 +1,33 @@
+package ru.practicum.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ ewm-main-service/src/main/java/ru/practicum/service/UserService.java
@@ -0,0 +1,14 @@
+package ru.practicum.service;
+
+import ru.practicum.dto.AdminUserDto;
+import ru.practicum.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    AdminUserDto createUser(User user);
+
+    void deleteUser(Long userId);
+
+    List<User> readUsers(List<Long> idList, int from, int size);
+}
