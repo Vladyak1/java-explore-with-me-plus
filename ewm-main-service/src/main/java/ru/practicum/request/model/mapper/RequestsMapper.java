bad0c6151fc69af0cc6cd199439c2a17766145b4
Степан Бокарев <bokarevstepan1@yandex.ru>
2024-09-22 21:45:02 +0300

    Добавить бин RequestsMapper

--- ewm-main-service/src/main/java/ru/practicum/request/model/mapper/RequestsMapper.java
+++ ewm-main-service/src/main/java/ru/practicum/request/model/mapper/RequestsMapper.java
@@ -3,10 +3,11 @@ package ru.practicum.request.model.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+import org.springframework.stereotype.Component;
 import ru.practicum.request.dto.ParticipationRequestDto;
 import ru.practicum.request.model.ParticipationRequest;
 
-
+@Component
 @Mapper
 public interface RequestsMapper {
     RequestsMapper REQUESTS_MAPPER = Mappers.getMapper(RequestsMapper.class);
