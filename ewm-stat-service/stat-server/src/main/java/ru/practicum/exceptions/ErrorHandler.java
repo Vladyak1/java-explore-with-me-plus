4c3bf31392d80fc2ec3c5f2b04c0966a18fd0c88
Vlad Yakovlev <vkysnoteevo1@mail.ru>
2024-09-25 19:56:32 +0300

    Правка сервиса статистики

--- ewm-stat-service/stat-server/src/main/java/ru/practicum/exceptions/ErrorHandler.java
+++ ewm-stat-service/stat-server/src/main/java/ru/practicum/exceptions/ErrorHandler.java
@@ -2,6 +2,7 @@ package ru.practicum.exceptions;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -28,6 +29,14 @@ public class ErrorHandler {
     public Map<String, String> errorBadRequest(final DataTimeException e) {
         log(e);
         return Map.of("error", e.getClass().getSimpleName(),
+                "errorMessage", e.getMessage());
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> errorBadRequest(final MissingServletRequestParameterException e) {
+        log(e);
+        return Map.of("error", "MissingServletRequestParameterException",
                 "errorMessage", e.getMessage());
     }
 }
